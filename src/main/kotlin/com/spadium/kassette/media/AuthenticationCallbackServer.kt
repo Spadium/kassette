f99abcb59929d4b030a1f9468b007d5a325c2959
Spadium <spadium.epic@gmail.com>
2025-06-17 15:29:22 -0500

    implement authentication stuff for spotify and other services (maybe)

---
+++ src/main/kotlin/com/spadium/kassette/media/AuthenticationCallbackServer.kt
@@ -0,0 +1,38 @@
+package com.spadium.kassette.media
+
+import com.sun.net.httpserver.HttpExchange
+import com.sun.net.httpserver.HttpHandler
+import com.sun.net.httpserver.HttpServer
+import java.io.OutputStream
+import java.net.InetSocketAddress
+
+class AuthenticationCallbackServer {
+    var server: HttpServer;
+
+    constructor() {
+        server = HttpServer.create(InetSocketAddress(61008), 0)
+        server.createContext("/callback", AuthCallbackHandler())
+        server.createContext("/favicon.ico", FaviconHandler())
+        server.executor = null
+        server.start()
+    }
+
+    class FaviconHandler: HttpHandler {
+        override fun handle(exchange: HttpExchange?) {
+            exchange?.sendResponseHeaders(200, 0)
+            val outputStream: OutputStream? = exchange?.responseBody
+            outputStream?.write(byteArrayOf())
+            outputStream?.close()
+        }
+    }
+
+    class AuthCallbackHandler: HttpHandler {
+        override fun handle(exchange: HttpExchange?) {
+            val response: String = "<h1>you're safe for now</h1>"
+            exchange?.sendResponseHeaders(200, response.length.toLong())
+            val outStream: OutputStream? = exchange?.responseBody
+            outStream?.write(response.toByteArray())
+            outStream?.close()
+        }
+    }
+}
