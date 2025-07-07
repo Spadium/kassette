964028d3739a95d87e7939099be17f04b84f0205
Spadium <spadium.epic@gmail.com>
2025-07-06 23:15:23 -0500

    media provider changes

--- src/main/kotlin/com/spadium/kassette/media/MediaProvider.kt
+++ src/main/kotlin/com/spadium/kassette/media/MediaProvider.kt
@@ -1,11 +1,15 @@
 package com.spadium.kassette.media
 
 abstract class MediaProvider {
+    constructor() {
+        init()
+    }
+
     open var state: MediaManager.MediaState = MediaManager.MediaState.OTHER
 
     abstract fun getServiceName(): String
 
-    abstract fun init()
+    fun init() {}
 
     abstract fun destroy()
 
