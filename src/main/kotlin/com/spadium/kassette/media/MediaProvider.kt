485a57b692e913a8440397855cc63e676621d274
Spadium <spadium.epic@gmail.com>
2025-07-06 23:23:28 -0500

    deprecate MediaProvider::init()

--- src/main/kotlin/com/spadium/kassette/media/MediaProvider.kt
+++ src/main/kotlin/com/spadium/kassette/media/MediaProvider.kt
@@ -2,14 +2,17 @@ package com.spadium.kassette.media
 
 abstract class MediaProvider {
     constructor() {
-        init()
+        this.init()
     }
 
     open var state: MediaManager.MediaState = MediaManager.MediaState.OTHER
 
     abstract fun getServiceName(): String
-
-    fun init() {}
+    @Deprecated(
+        "Constructors will initialize providers since git commit 964028d3! This will be removed by release!",
+        ReplaceWith("constructor()")
+    )
+    open fun init() {}
 
     abstract fun destroy()
 
