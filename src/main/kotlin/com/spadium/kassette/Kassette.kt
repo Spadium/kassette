e4cf7de88067cef2ba65ec040c95fc36419a7300
Spadium <spadium.epic@gmail.com>
2025-07-07 12:36:51 -0500

    formatting and remove the beginning of a hack

--- src/main/kotlin/com/spadium/kassette/Kassette.kt
+++ src/main/kotlin/com/spadium/kassette/Kassette.kt
@@ -1,5 +1,7 @@
 package com.spadium.kassette
 
+import com.mojang.blaze3d.opengl.GlConst
+import com.mojang.blaze3d.pipeline.RenderPipeline
 import com.spadium.kassette.config.Config
 import com.spadium.kassette.media.AuthenticationCallbackServer
 import com.spadium.kassette.media.DebugProvider
@@ -7,7 +9,6 @@ import com.spadium.kassette.media.MediaManager
 import com.spadium.kassette.media.PlaceholderProvider
 import com.spadium.kassette.ui.MediaInfoHUD
 import com.spadium.kassette.ui.MediaInfoScreen
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager
@@ -15,9 +16,8 @@ import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallba
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
-import net.fabricmc.fabric.api.client.rendering.v1.AtlasSourceRegistry
-import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.client.MinecraftClient
+import net.minecraft.client.gl.RenderPipelines
 import net.minecraft.client.option.KeyBinding
 import net.minecraft.client.util.InputUtil
 import net.minecraft.text.Text
@@ -25,63 +25,64 @@ import net.minecraft.util.Identifier
 import org.lwjgl.glfw.GLFW
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import javax.imageio.ImageIO
 import kotlin.concurrent.thread
 
 
 open class Kassette : ClientModInitializer {
-	companion object {
-		val logger: Logger = LoggerFactory.getLogger("Kassette")
-	}
+    companion object {
+        val logger: Logger = LoggerFactory.getLogger("Kassette")
+    }
 
     override fun onInitializeClient() {
-		try {
-			AuthenticationCallbackServer().start()
-		} catch (e: Exception) {
-			logger.error("Error initializing Kassette's authentication callback!", e)
-		}
+        try {
+            AuthenticationCallbackServer().start()
+        } catch (e: Exception) {
+            logger.error("Error initializing Kassette's authentication callback!", e)
+        }
 
-		Config.Instance = Config.reload()
+        Config.Instance = Config.reload()
 
-		val openMediaInfoKeybind: KeyBinding = KeyBindingHelper.registerKeyBinding(KeyBinding(
-			"key.kassette.info",
-			InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_M,
-			"category.kassette.kassette"
-		))
+        val openMediaInfoKeybind: KeyBinding = KeyBindingHelper.registerKeyBinding(
+            KeyBinding(
+                "key.kassette.info",
+                InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_M,
+                "category.kassette.kassette"
+            )
+        )
 
-		ClientTickEvents.END_CLIENT_TICK.register(ClientTickEvents.EndTick { client: MinecraftClient? ->
-			while (openMediaInfoKeybind.wasPressed()) {
-				client!!.setScreen(
-					MediaInfoScreen(Text.translatable("kassette.popup.title"))
-				)
-			}
-		})
+        ClientTickEvents.END_CLIENT_TICK.register(ClientTickEvents.EndTick { client: MinecraftClient? ->
+            while (openMediaInfoKeybind.wasPressed()) {
+                client!!.setScreen(
+                    MediaInfoScreen(Text.translatable("kassette.popup.title"))
+                )
+            }
+        })
 
-		ClientCommandRegistrationCallback.EVENT.register { dispatcher, registryAccess ->
-			dispatcher.register(ClientCommandManager.literal("kassette").executes { ctx ->
-				ctx.source.sendFeedback(Text.literal("Kassette command"))
+        ClientCommandRegistrationCallback.EVENT.register { dispatcher, registryAccess ->
+            dispatcher.register(ClientCommandManager.literal("kassette").executes { ctx ->
+                ctx.source.sendFeedback(Text.literal("Kassette command"))
                 return@executes 1
-			})
-		}
+            })
+        }
 
-		ClientLifecycleEvents.CLIENT_STARTED.register { client ->
-			MediaInfoHUD().setup()
-			MediaManager.provider = DebugProvider()
-			MediaManager.provider.init()
-			thread(name = "Kassette MediaManager Thread") {
-				while (client.isRunning) {
-						runBlocking {
-							try {
-								MediaManager.provider.update()
-							} catch (e: Exception) {
-								e.printStackTrace()
-								logger.warn("Exception thrown by provider for \"${MediaManager.provider.getServiceName()}\"! Using PlaceholderProvider as fallback!")
-								MediaManager.provider = PlaceholderProvider()
-							}
-						}
-				}
-
-			}
-		}
-		logger.info("Locked and loaded")
-	}
+        ClientLifecycleEvents.CLIENT_STARTED.register { client ->
+            MediaInfoHUD().setup()
+            MediaManager.provider = DebugProvider()
+            thread(name = "Kassette MediaManager Thread") {
+                while (client.isRunning) {
+                    try {
+                        runBlocking {
+                            MediaManager.provider.update()
+                        }
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                        logger.warn("Exception thrown by provider for \"${MediaManager.provider.getServiceName()}\"! Using PlaceholderProvider as fallback!")
+                        MediaManager.provider = PlaceholderProvider()
+                    }
+                }
+            }
+        }
+        logger.info("Locked and loaded")
+    }
 }
