ca0adf10606fc354bfbea416e026b72cc8ed57e5
Spadium <spadium.epic@gmail.com>
2025-07-13 23:35:19 -0500

    delete unused code

--- src/main/kotlin/com/spadium/kassette/util/ImageUtils.kt
+++ src/main/kotlin/com/spadium/kassette/util/ImageUtils.kt
@@ -13,19 +13,6 @@ import javax.imageio.ImageIO
 
 class ImageUtils {
     companion object {
-        @JvmStatic
-        fun validateImage(buffer: ByteBuffer, originalOrder: ByteOrder) {
-            buffer.order(ByteOrder.BIG_ENDIAN)
-
-            if (buffer.getLong(0) == -10977661529601466) {
-                buffer.rewind()
-            } else {
-                buffer.rewind()
-                buffer.order(originalOrder)
-                PngMetadata.validate(buffer)
-            }
-        }
-
         fun loadImageIOImage(input: InputStream, close: Boolean = false): NativeImage {
             val bufferedImage: BufferedImage = ImageIO.read(input)
             val nativeImage: NativeImage = NativeImage(bufferedImage.width, bufferedImage.height, true)
@@ -49,34 +36,6 @@ class ImageUtils {
             val a = (rgb shr 24) and 0xFF
 
             return (a shl 24) or (b shl 16) or (g shl 8) or r
-        }
-
-        fun loadGenericImage(bytes: ByteArray): NativeImage {
-            return loadGenericImage(bytes, NativeImage.Format.RGBA)
-        }
-
-        fun loadGenericImage(bytes: ByteArray, format: NativeImage.Format): NativeImage {
-            val imgBuf = MemoryUtil.memAlloc(bytes.size)
-            imgBuf.put(bytes)
-            imgBuf.flip()
-            return loadStbImage(imgBuf, format)
-        }
-
-        fun loadStbImage(buf: ByteBuffer, format: NativeImage.Format): NativeImage {
-            val widthBuf = MemoryUtil.memAllocInt(1)
-            val heightBuf = MemoryUtil.memAllocInt(1)
-            val channelBuf = MemoryUtil.memAllocInt(1)
-            val imgBuf = STBImage.stbi_load_from_memory(
-                buf, widthBuf, heightBuf, channelBuf, format.channelCount
-            )
-            if (imgBuf == null) {
-                throw IOException("Failed to decode image / null buffer! Reason: ${STBImage.stbi_failure_reason()}")
-            }
-            return NativeImage(
-                format,
-                widthBuf.get(0), heightBuf.get(0),
-                false, MemoryUtil.memAddress(imgBuf)
-            )
         }
     }
 }
