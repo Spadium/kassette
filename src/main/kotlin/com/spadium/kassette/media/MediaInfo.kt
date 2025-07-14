6631b0c58977661d9545480622867451044d6afc
Spadium <spadium.epic@gmail.com>
2025-07-13 23:00:57 -0500

    change media info equals function

--- src/main/kotlin/com/spadium/kassette/media/MediaInfo.kt
+++ src/main/kotlin/com/spadium/kassette/media/MediaInfo.kt
@@ -11,4 +11,22 @@ data class MediaInfo(
     var coverArt: NativeImage,
     var provider: String,
     var state: MediaManager.MediaState = MediaManager.MediaState.OTHER
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as MediaInfo
+        // ignore currentPosition for comparisons
+        if (maximumTime != other.maximumTime) return false
+        if (title != other.title) return false
+        if (album != other.album) return false
+        if (artist != other.artist) return false
+        if (coverArt != other.coverArt) return false
+        if (provider != other.provider) return false
+        if (state != other.state) return false
+
+        return true
+    }
+
+}
