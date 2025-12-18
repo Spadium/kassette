9c0f56aa404ce183d6188e0d78b857b5033c7b9e
Spadium <spadium.epic@gmail.com>
2025-12-17 18:01:45 -0600

    slight changes to names and stuff

--- src/main/java/com/spadium/kassette/mixin/TemporaryConfigPathMixin.java
+++ src/main/java/com/spadium/kassette/mixin/TemporaryConfigPathMixin.java
@@ -1,15 +1,42 @@
 package com.spadium.kassette.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import com.spadium.kassette.ui.screens.config.ConfigScreen;
+import net.minecraft.client.Options;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.layouts.GridLayout;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.gui.screens.options.OptionsScreen;
+import net.minecraft.network.chat.Component;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.function.Supplier;
 
 @Mixin(OptionsScreen.class)
-public class TemporaryConfigPathMixin {
-    private void createKassetteConfigButton() {
+public class TemporaryConfigPathMixin extends Screen {
+    protected TemporaryConfigPathMixin(Component title) {
+        super(title);
+    }
 
+    @Shadow
+    private Button openScreenButton(Component text, Supplier<Screen> screenSupplier) {
+        throw new AssertionError();
+    }
+
+    @Inject(
+        method = "init",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/layouts/HeaderAndFooterLayout;addToContents(Lnet/minecraft/client/gui/layouts/LayoutElement;)Lnet/minecraft/client/gui/layouts/LayoutElement;"
+        )
+    )
+    private void createKassetteConfigButton(CallbackInfo ci, @Local LocalRef<Button> button, @Local GridLayout.RowHelper rowHelper) {
+        button.set(
+                rowHelper.addChild(openScreenButton(Component.literal("Kassette Config"), () -> new ConfigScreen((OptionsScreen)(Object)this) ))
+        );
     }
 }
--- src/main/kotlin/com/spadium/kassette/ui/screens/config/ConfigScreen.kt
+++ src/main/kotlin/com/spadium/kassette/ui/screens/config/ConfigScreen.kt
@@ -18,7 +18,7 @@ import net.minecraft.resources.Identifier
 
 class ConfigScreen : Screen {
     val parent: Screen?
-    val layout = HeaderAndFooterLayout(this, 32)
+    val layout: HeaderAndFooterLayout = HeaderAndFooterLayout(this, 32)
     lateinit var sections: LayoutListWidget
 
     constructor(parent: Screen?) : super(Component.translatable("kassette.config.title")) {
@@ -86,18 +86,18 @@ class ConfigScreen : Screen {
         )
 
         layout.visitWidgets { widget ->
-            addWidget(widget)
+            addRenderableWidget(widget)
         }
-//        repositionElements()
+        repositionElements()
     }
 
-//    override fun repositionElements() {
-//        layout.arrangeElements()
-//        sections.updateSize(width, layout)
-//    }
+    override fun repositionElements() {
+        layout.arrangeElements()
+        sections.updateSize(width, layout)
+    }
 
-//    override fun onClose() {
-//        MainConfig.Instance.save()
-//        this.minecraft.setScreen(parent)
-//    }
+    override fun onClose() {
+        MainConfig.Instance.save()
+        this.minecraft.setScreen(parent)
+    }
 }
